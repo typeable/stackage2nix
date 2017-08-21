52f3b66ec5702c501a1a8b3e36dc40c459d42678
Dmitry Bushev <bushevdv@gmail.com>
2017-08-21 20:02:56 +0300

    Add fallback load package from Hackage Db

--- src/LtsHaskell.hs
+++ src/LtsHaskell.hs
@@ -2,14 +2,17 @@ module LtsHaskell where
 
 import AllCabalHashes
 import Control.Lens
+import Control.Monad.Catch
 import Data.Maybe
-import Distribution.Text (display)
+import Data.Text as T
+import Distribution.Text as Text (display)
 import Control.Monad
 import Distribution.Compiler (CompilerInfo(..), AbiTag(NoAbiTag), CompilerId(..), CompilerFlavor(GHC))
 import Distribution.System (Platform(..))
 import Distribution.Package (PackageName, PackageIdentifier(..), Dependency(..))
-import Distribution.Nixpkgs.Haskell.PackageSourceSpec
+import Distribution.Nixpkgs.Haskell.PackageSourceSpec as PackageSourceSpec
 import Distribution.Nixpkgs.Haskell.FromStack
+import Distribution.Nixpkgs.Haskell.Stack
 import Distribution.Nixpkgs.Fetch
 import Distribution.Nixpkgs.PackageMap (readNixpkgPackageMap, resolve)
 import Distribution.Version (Version, withinRange)
@@ -30,10 +33,12 @@ loadBuildPlan = Yaml.decodeFileEither >=> \case
   Left err -> fail $ "Failed to parse stackage build plan: " ++ show err
   Right bp -> return bp
 
-loadPackage :: FilePath -> Maybe SHA1Hash -> PackageIdentifier -> IO Package
-loadPackage allCabalHashesPath mSha1Hash pkgId = do
+getPackageFromRepo :: FilePath -> Maybe SHA1Hash -> PackageIdentifier -> IO Package
+getPackageFromRepo allCabalHashesPath mSha1Hash pkgId = do
   (pkgDesc, _) <- case mSha1Hash of
-    Just sha1 -> readPackageByHash allCabalHashesPath sha1
+    Just sha1 ->
+      readPackageByHash allCabalHashesPath sha1
+      `catchAll` const (readPackageByName allCabalHashesPath pkgId)
     Nothing   -> readPackageByName allCabalHashesPath pkgId
   meta <- readPackageMeta allCabalHashesPath pkgId
   let
@@ -42,6 +47,16 @@ loadPackage allCabalHashesPath mSha1Hash pkgId = do
       (view (mHashes . at "SHA256") meta)
     source = DerivationSource "url" ("mirror://hackage/" ++ display pkgId ++ ".tar.gz") "" tarballSHA256
   return $ Package source pkgDesc
+
+getPackageFromDb :: PackageIdentifier -> IO Package
+getPackageFromDb pkgId =
+  getStackPackageFromDb Nothing
+  $ StackPackage (HackagePackage (T.pack $ Text.display pkgId)) True
+
+loadPackage :: FilePath -> Maybe SHA1Hash -> PackageIdentifier -> IO Package
+loadPackage allCabalHashesPath mSha1Hash pkgId =
+  getPackageFromRepo allCabalHashesPath mSha1Hash pkgId
+  `catchIOError` const (getPackageFromDb pkgId)
 
 ghcCompilerInfo :: Version -> CompilerInfo
 ghcCompilerInfo v = CompilerInfo
@@ -56,7 +71,11 @@ buildPlanContainsDependency :: Map.Map PackageName Version -> Dependency -> Bool
 buildPlanContainsDependency packageVersions (Dependency depName versionRange) =
   maybe False (`withinRange` versionRange) $ Map.lookup depName packageVersions
 
-buildPackageSetConfig :: FilePath -> FilePath -> BuildPlan -> IO PackageSetConfig
+buildPackageSetConfig
+  :: FilePath
+  -> FilePath
+  -> BuildPlan
+  -> IO PackageSetConfig
 buildPackageSetConfig optAllCabalHashes optNixpkgsRepository buildPlan = do
   nixpkgs <- readNixpkgPackageMap optNixpkgsRepository Nothing
   let
